2af7c9550df300b6d03aaba5a91a298e290745d3
kevinlul <6320810+kevinlul@users.noreply.github.com>
2023-05-02 18:52:59 -0400

    Remove sponsorship links for Reddit spam filter and per /r/yugioh moderator request (rule 7)

--- src/footer.py
+++ src/footer.py
@@ -6,6 +6,4 @@ FOOTER = """
 
 ^by ^[/u/BastionBotDev](/u/BastionBotDev) ^|
 ^[GitHub](https://github.com/DawnbrandBots/bastion-for-reddit) ^|
-^Licence: ^[GNU&nbsp;AGPL&nbsp;3.0+](https://choosealicense.com/licenses/agpl-3.0/) ^|
-^[Patreon](https://www.patreon.com/alphakretinbots) ^|
-^[Ko-fi](https://ko-fi.com/dawnbrandbots)"""
+^Licence: ^[GNU&nbsp;AGPL&nbsp;3.0+](https://choosealicense.com/licenses/agpl-3.0/)"""
