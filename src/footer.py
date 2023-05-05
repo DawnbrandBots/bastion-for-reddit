d252bca59feb71ce618ea03abbd699ba6244034d
kevinlul <6320810+kevinlul@users.noreply.github.com>
2023-05-05 18:29:19 -0400

    Fix footer mentions again

--- src/footer.py
+++ src/footer.py
@@ -4,6 +4,6 @@ FOOTER = """
 
 ----
 
-^by ^[u&#47;BastionBotDev](/u/BastionBotDev) ^|
+^by ^[u&#47;BastionBotDev](/user/BastionBotDev) ^|
 ^[GitHub](https://github.com/DawnbrandBots/bastion-for-reddit) ^|
 ^Licence: ^[GNU&nbsp;AGPL&nbsp;3.0+](https://choosealicense.com/licenses/agpl-3.0/)"""
